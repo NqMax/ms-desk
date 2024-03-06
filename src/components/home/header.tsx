28f36bfeb5f3cf330e4a51de8043c38b71544ffe
Max <111196993+NqMax@users.noreply.github.com>
2024-03-05 23:31:21 -0600

    Initial Header UI

---
+++ src/components/home/header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { HeaderNav } from "@/components/home/headerNav";
+import { SignInButton } from "@/components/home/signInButton";
+
+export function Header() {
+  return (
+    <header className="border-b">
+      <div className="container py-2 flex justify-between items-center">
+        <Link href="/" className="flex items-center h-[60px]">
+          <div className="text-4xl text-foreground font-satisfy">MS Desk</div>
+        </Link>
+        <HeaderNav />
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
