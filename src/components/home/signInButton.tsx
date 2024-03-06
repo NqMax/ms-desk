01828efdc4a38496fa93708748b5509c33df74ee
Max <111196993+NqMax@users.noreply.github.com>
2024-03-05 23:32:21 -0600

    Implemented sign in button UI

---
+++ src/components/home/signInButton.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { CircleUserRound } from 'lucide-react';
+import Link from "next/link";
+
+export function SignInButton() {
+  return (
+    <Button asChild className="gap-x-3">
+      <Link href="/administration">
+        <span>Join </span>
+        <span className="text-xs">|</span>
+        <span>Sign In</span>
+        <CircleUserRound size={24} />
+      </Link>
+    </Button>
+  );
+}
