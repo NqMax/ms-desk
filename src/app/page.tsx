9411a3fc9908548ebb5e1c7178e9a54a7d9199ab
Max <111196993+NqMax@users.noreply.github.com>
2024-03-20 00:25:24 -0600

    Added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
+// UI Components
+import { Button } from "@/components/ui/button";
+// Icons
+import { TicketCheck } from "lucide-react";
 
 export default function Home() {
   return (
@@ -12,7 +17,54 @@ export default function Home() {
           className="object-cover -z-10"
           priority
         />
-        {/* <SearchBar /> */}
+        <div className="flex container items-center justify-center">
+          <div className="flex flex-col gap-y-3 bg-background rounded-lg border p-4">
+            <h1 className="flex text-lg font-bold items-center">
+              Welcome to Microsoft Desk{" "}
+              <TicketCheck className="h-6 w-6 ml-3 shrink-0" />
+            </h1>
+            <p>
+              Get the help you need quickly and easily with our intuitive
+              ticketing system. Submit tickets, track their progress, and
+              communicate directly with our team, all in one place.
+            </p>
+            <div className="grid grid-cols-3 gap-x-5 mt-3 mb-5">
+              <div className="flex flex-col gap-y-2">
+                <div className="font-bold">Submit Tickets</div>
+                <Image
+                  src="/h-1.png"
+                  alt="Icon 1"
+                  width={3084}
+                  height={1926}
+                  quality={100}
+                />
+              </div>
+              <div className="flex flex-col gap-y-2">
+                <div className="font-bold">Get Instant Support</div>
+                <Image
+                  src="/h-1.png"
+                  alt="Icon 1"
+                  width={3084}
+                  height={1926}
+                  quality={100}
+                />
+              </div>
+              <div className="flex flex-col gap-y-2">
+                <div className="font-bold">Track Progress</div>
+                <Image
+                  src="/h-1.png"
+                  alt="Icon 1"
+                  width={3084}
+                  height={1926}
+                  quality={100}
+                />
+              </div>
+            </div>
+            <Button asChild>
+              <Link href="/dashboard">Get Started</Link>
+            </Button>
+          </div>
+        </div>
       </section>
       {/* <Footer /> */}
     </>
