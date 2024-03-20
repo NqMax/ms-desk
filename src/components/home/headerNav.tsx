78da3bb5ef1e82fc20c21083b8ef761322904111
Max <111196993+NqMax@users.noreply.github.com>
2024-03-20 00:24:30 -0600

    Added header nav component

--- src/components/home/headerNav.tsx
+++ src/components/home/headerNav.tsx
@@ -11,38 +11,93 @@ import {
 } from "@/components/ui/navigation-menu";
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 import Link from "next/link";
+import { Ticket } from "lucide-react";
 
 export function HeaderNav() {
   return (
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Hotels & Resorts</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Getting Started</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <NavigationMenuLink
-              asChild
-              className={navigationMenuTriggerStyle()}
-            >
-              <Link href="/">Coming Soon!</Link>
-            </NavigationMenuLink>
+            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <Link
+                    href="/"
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                  >
+                    <Ticket className="h-6 w-6" />
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      Microsoft Desk
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Get the help you need quickly and easily with our
+                      intuitive ticketing system. Submit tickets, track their
+                      progress, and communicate directly with our support team
+                      all in one place.
+                    </p>
+                  </Link>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/docs/user" title="Submit a Ticket">
+                A comprehensive guide to submitting a ticket within Microsoft
+                Desk.
+              </ListItem>
+              <ListItem href="/docs/user" title="Ticket Priority">
+                Microsoft Desk offers a range of ticket priorities to help you
+                get the help you need quickly.
+              </ListItem>
+              <ListItem href="/docs/user" title="Ticket System">
+                Learn more about the ticket system and how it works.
+              </ListItem>
+            </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link href="/">Special Offers</Link>
+            <Link href="/docs/user">Customer</Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link href="/">Partners</Link>
+            <Link href="/docs/administrator">Administrator</Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link href="/">About Zen Hotels</Link>
+            <Link href="/about-us">About Microsoft Desk</Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
+  );
+}
+
+function ListItem({
+  title,
+  children,
+  href,
+}: {
+  title: string;
+  children: React.ReactNode;
+  href: string;
+}) {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <Link
+          href={href}
+          className={
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+          }
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </Link>
+      </NavigationMenuLink>
+    </li>
   );
 }
