3170bf1cd8eb0e0c695adf7e2ff8b75cb1f86dfa
Max <111196993+NqMax@users.noreply.github.com>
2024-03-20 00:25:04 -0600

    Added header component

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,13 +1,22 @@
 import Link from "next/link";
+import Image from "next/image";
+// Components
 import { HeaderNav } from "@/components/home/headerNav";
 import { SignInButton } from "@/components/home/signInButton";
 
 export function Header() {
   return (
     <header className="border-b">
-      <div className="container py-2 flex justify-between items-center">
-        <Link href="/" className="flex items-center h-[60px]">
-          <div className="text-4xl text-foreground font-satisfy">MS Desk</div>
+      <div className="container py-2 flex justify-between items-center ">
+        <Link href="/" className="flex items-center gap-x-3 h-[60px]">
+          <Image
+            src={`/logo.png`}
+            alt="Microsoft Desk Logo"
+            width="30"
+            height="30"
+            priority
+          />
+          <div className="text-xl font-bold">Microsoft Desk</div>
         </Link>
         <HeaderNav />
         <SignInButton />
